ce381ef21a2acf46c2ab1cab7b31303180cf13f6
tim <43515959+timhunderwood@users.noreply.github.com>
2019-06-04 16:15:28 +0100

    bose einstein condensate example

---
+++ examples/bec_example.py
@@ -0,0 +1,29 @@
+import numpy
+import imageio
+import matplotlib.pyplot as plt
+import numpystl
+
+
+
+def convert_raw_image(file_name):
+    array=imageio.imread(file_name)
+    cropped=array[210:-240,210:-240]
+    imageio.imwrite(file_name[0:-4]+"_cropped.png", cropped)
+
+def normalise_camera_image(array):
+    scale = 10089.33
+    offset = 5000.0
+    optical_density = (array - offset) / scale
+    optical_density = numpy.clip(optical_density, a_min=0, a_max=4)
+    z_scale_factor = 0.5*max(array.shape)/optical_density.max()
+    scaled_array = z_scale_factor*optical_density
+    return scaled_array
+
+if __name__=="__main__":
+    convert_raw_image("raw_075.png")
+    raw = imageio.imread("raw_075_cropped.png")
+    od = normalise_camera_image(raw)
+    mesh = numpystl.create_stl_mesh_from_2d_array(od, base_height=5)
+    plt.matshow(od)
+    plt.show()
+    mesh.save("bec.stl")
